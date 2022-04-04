e076e63e98737ccdb6ba4947221a9df5cfa12a38
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2022-04-04 21:28:30 +0200

    Finished API

--- models/subscriber.js
+++ models/subscriber.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const subscriberSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  subscriberToChannel: { type: String, required: true },
+  subscribedToChannel: { type: String, required: true },
   subscribeDate: { type: Date, required: true, default: Date.now },
 });
 
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -4,7 +4,7 @@ const Subscriber = require('../models/subscriber');
 
 const router = express.Router();
 
-// Getting all
+// Getting ALL
 router.get('/', async (req, res) => {
   try {
     const subscribers = await Subscriber.find();
@@ -14,17 +14,63 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Getting one
-router.get('/:id', (req, res) => {
-  res.send(req.params.id);
+// Getting One
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber);
 });
-// Creating one
-router.post('/', (req, res) => {});
 
-// Updating one
-router.patch('/', (req, res) => {});
+// Creating one
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Updating One
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Deleting one
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: 'Deleted Subscriber' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
